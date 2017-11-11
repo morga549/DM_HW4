746b09ecb993e7af9c9d21bf3474e6f10b9e661d
Jack Morgan <jack.art.morgan@gmail.com>
2017-11-10 20:53:07 -0600

    Added comments.

--- john.morgan.2Assig4.py
+++ john.morgan.2Assig4.py
@@ -15,10 +15,9 @@ for line in file:
     if(int(v) > maxNode): maxNode = int(v)
 file.close()
 
-# build adjacency matrix of graph
+#build a list of lists representing an adjacency matrix
 adjMatrix = np.zeros((maxNode, maxNode), dtype = np.int)
 
-# fill the adjacency matrix
 file = open(infile, 'r')
 for line in file:
     u,v = line.split()
@@ -28,41 +27,39 @@ for line in file:
     adjMatrix[iv][iu] = 1
 file.close()
 
-# calculates the degree distribution of a graph
 def degreeDist(matrix):
-    nodeDegrees = reduce(lambda x,y: x+y, matrix[:]) # list containing the degree of each node in the graph
-    frequencies = Counter(nodeDegrees).values() # get the frequencies of each degree
-    keys = Counter(nodeDegrees).keys() # degree associated with each frequency
+    nodeDegrees = reduce(lambda x,y: x+y, matrix[:])
+    frequencies = Counter(nodeDegrees).values()
+    keys = Counter(nodeDegrees).keys()
     plt.scatter(keys, frequencies)
     plt.show()
 
-    probabilies = [(float(x) / sum(frequencies)) for x in frequencies] # list containing the probabilies of each degree frequency
+    probabilies = [(float(x) / sum(frequencies)) for x in frequencies]
     plt.scatter(keys, probabilies)
     plt.xscale('log')
     plt.yscale('log')
     plt.show()
     return zip(keys, frequencies)
 
-# calculates the clustering Coefficient of a graph
 def clusteringCoeff(matrix):
     friends = []
 
     for row in matrix[:]:
-         friends.append(np.where(row == 1)) # list of neighbors for each node
+         friends.append(np.where(row == 1))
 
     globalCC = 0.0
 
     for node in friends:
-        degree = len(node[0]) # number of neighbors you have
-        triangles = 0 # of connections between neighbors
+        degree = len(node[0])
+        triangles = 0
 
         for x in node[0]:
             triangles += len(np.intersect1d(friends[x][0], node[0]))
         if(degree > 1):
-            localCC = float(triangles) / (degree * (degree - 1)) # Clustering Coefficient of the current node
+            localCC = float(triangles) / (degree * (degree - 1))
             globalCC += localCC
 
-    globalCC /= matrix[0].shape[0] # average Clustering Coefficient of the graph
+    globalCC /= matrix[0].shape[0]
     return globalCC
 
 print "Clustering Coefficient: "
